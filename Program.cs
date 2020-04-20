c532ff072175ad247009b0cb6cfd2da5f4e3c3fb
Sobira <sobira204@gmail.com>
2020-04-20 20:57:30 +0500

    initialisation project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace delagate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
